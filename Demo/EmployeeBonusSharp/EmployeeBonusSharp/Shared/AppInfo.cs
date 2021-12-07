7fbb65f0e7eba98e2543d7812c1d766d3ffd3828
Marko Lohert <lohert.marko@gmail.com>
2021-12-07 20:43:41 +0100

    About page added.

--- Demo/EmployeeBonusSharp/EmployeeBonusSharp/Shared/AppInfo.cs
+++ Demo/EmployeeBonusSharp/EmployeeBonusSharp/Shared/AppInfo.cs
@@ -1,10 +1,17 @@
-﻿namespace EmployeeBonusSharp.Shared
+﻿using System;
+
+namespace EmployeeBonusSharp.Shared
 {
     public record AppInfo
     {
-        public const string AppName = "EmployeeBonus #";
+        public const string AppName = "EmployeeBonus#";
         public const string LicenseName = "MIT License";
 
         public string BasicInfo = $"{AppName} licensed under {LicenseName}";
+
+        public string GetCompleteAboutMessage()
+        {
+            return BasicInfo = $"\nversion 2021.1";
+        }
     }
 }
